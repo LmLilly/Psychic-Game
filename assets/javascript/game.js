7b7ab06ff4214ddbf931c20c0b631018565694c1
LmLilly <lillym@Lidiettes-MacBook-Pro.local>
2017-04-25 21:31:01 -0700

    added jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,20 @@
 //List of choices for the computer with an array
-var options = ["a"];
+var options = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var GuessesLeft = 9
 
 //Variable to hold wins and looses
 var wins = 0;
 var losses = 0;
-
+var computerGuess = options[Math.floor(Math.random()*26)];
+							//2.math,floor makes the number round down
+console.log(computerGuess)
 
 	//User 
 	document.onkeyup = function() {
 		var userguess = String.fromCharCode(event.keyCode).
 			toLowerCase(); //all keys are lower cases
 		console.log (userguess);
-	//var computerGuess = options[Math.floor(Math.random()*options.length)];
-							//2.math,floor makes the number round down
-	var computerGuess= "a"
-	console.log(computerGuess);
+							
 	if (computerGuess === userguess){
 		//wins
 		console.log("user wins")
@@ -23,13 +22,19 @@ var losses = 0;
 	} 
 	else {
 		//loose
-		alert("try again")
+		//alert("try again")
 		losses = losses + 1
 	}
 	GuessesLeft = GuessesLeft - 1
+
 	console.log(GuessesLeft)
 	console.log(wins)
 	console.log(losses)
+
+	$("#Wins .count").text(wins)
+	$("#Losses .count").text(losses)
+	$("#GuessesLeft .count").text(GuessesLeft)
+	$("#YourGuessesSoFar .count").append(userguess+", ")
 
 
 }
